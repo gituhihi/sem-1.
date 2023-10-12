0611757ec7999a9537581eacc799cd4635ea2928
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2023-10-12 17:16:59 +0530

    Add files via upload

---
+++ Lab 13/13-B-1-b.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int rows;
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            if ((i + j) % 2 == 0) {
+                printf("1 ");
+            } else {
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }
+}
